bf35afcc84a27c7bd2a1c00e4c62bf1f0413d314
Worasit Daimongkol <worasit.dmk501@gmail.com>
2020-02-10 10:10:33 +0700

    Added log

--- src/main/scala/com/nongped/graphqlserver/Server.scala
+++ src/main/scala/com/nongped/graphqlserver/Server.scala
@@ -1,5 +1,10 @@
 package com.nongped.graphqlserver
 
-object Server extends App {
-  
+import com.typesafe.scalalogging.LazyLogging
+
+object Server extends App with LazyLogging {
+  val port = 9000
+
+
+  logger.info(s"The server is running at: http://localhost:${port}")
 }
